6df0c45e5c4151fdccc15096486cdeae2de4687c
Eugene Domer <yewgeenn@gmail.com>
2021-03-24 12:19:04 +0200

    change models

--- profile/models.py
+++ profile/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField(max_length=50, unique=False)
     last_name = models.CharField(max_length=50, unique=False)
-    phone_number = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    phone_number = models.CharField(max_length=13, unique=True, null=False, blank=False)
     age = models.PositiveIntegerField(null=False, blank=False)
     GENDER_CHOICES = (
         ('M', 'Male'),
