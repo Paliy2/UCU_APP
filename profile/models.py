83f81f39e9db8ec6dccad735d692a98cacaf6978
Eugene Domer <yewgeenn@gmail.com>
2021-05-17 19:52:44 +0300

    ASDASD

--- profile/models.py
+++ profile/models.py
@@ -8,7 +8,7 @@ from user.models import User
 
 class PhoneNumber(models.Model):
 
-    id = models.AutoField(primary_key=True, db_index=True, unique=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     emailucu = models.EmailField()
     lastnameukr = models.CharField(max_length=1000, unique=False)
     firstnameukr = models.CharField(max_length=1000, unique=False)
